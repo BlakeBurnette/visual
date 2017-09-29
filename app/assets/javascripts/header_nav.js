b1bad7ce8de74cd4d3f28b474e8f2b690d8bda00
Blake <jbburne2@ncsu.edu>
2017-09-28 22:08:27 -0400

    updating whole image and idea

--- app/assets/javascripts/header_nav.js
+++ app/assets/javascripts/header_nav.js
@@ -2,3 +2,5 @@ var headerClick = function headerClick() {
   $("nav#toggle-nav ul").toggleClass("expanded");
   $("img#nav-plus-toggle").toggleClass("rotated");
 }
+
+setTimeout(function(){ headerClick() }, 1000);
